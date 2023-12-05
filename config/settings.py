5b75319e5b6263b696dbe1b23e21370d035684bc
Misha <misha.dorozhkin@mail.ru>
2023-12-05 20:35:03 +0300

    Создал CRUD для моделей Course(ViewSet) и Lesson(Generic)

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+
+    'study',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -97,3 +100,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
